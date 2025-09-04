411fa29247cb37bb1dc3ec6b96cb866089bd1743
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-09-03 21:05:32 -0300

    :tada: feat: cria estrutura inicial do projeto

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+//
+//  Main.cpp
+//  Trabalho IDJ 2025.2
+//
+//  Created by Maciel Ferreira Custódio Júnior on 03/09/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
