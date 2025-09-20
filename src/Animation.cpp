22adfa7db1e97b1431149a7905428bf5019001b6
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-09-20 15:04:11 -0300

    :sparkles: feat: adicionar classe Animation para controle de quadros de animação
    - Cria a classe Animation com atributos:
      - frameStart (int)
      - frameEnd (int)
      - frameTime (float)
    - Implementa construtor para inicializar os atributos.

---
+++ src/Animation.cpp
@@ -0,0 +1,14 @@
+//
+//  Animation.cpp
+//  Trabalho IDJ 2025.2
+//
+//  Created by Maciel Ferreira Custódio Júnior on 20/09/25.
+//
+
+#include "Animation.hpp"
+
+Animation::Animation(int frameStart, int frameEnd, float frameTime) {
+    this -> frameStart = frameStart;
+    this -> frameEnd = frameEnd;
+    this -> frameTime = frameTime;
+}
