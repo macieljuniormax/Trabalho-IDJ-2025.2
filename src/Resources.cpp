7d61b9f78c995a4a2109fb0ae09208f076199c78
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-10-04 17:58:09 -0300

    :sparkles: feat: adiciona classe Resources para gerenciamento de assets
    - Cria classe Resources com cache de texturas, músicas e sons.
    - Implementa métodos estáticos:
      - GetImage(), GetMusic(), GetSound() para carregar e reutilizar assets.
      - ClearImages(), ClearMusics(), ClearSounds() para liberar memória.

---
+++ src/Resources.cpp
@@ -0,0 +1,101 @@
+//
+//  Resources.cpp
+//  Trabalho IDJ 2025.2
+//
+//  Created by Maciel Ferreira Custódio Júnior on 04/10/25.
+//
+
+#include "Resources.hpp"
+#include "Game.hpp"
+
+#include <iostream>
+
+std::unordered_map<std::string, SDL_Texture *> Resources::imageTable;
+std::unordered_map<std::string, Mix_Music *> Resources::musicTable;
+std::unordered_map<std::string, Mix_Chunk *> Resources::soundTable;
+
+SDL_Texture *Resources::GetImage(const std::string &file) {
+    auto it = imageTable.find(file);
+
+    if (it != imageTable.end()) {
+        return it->second;
+    }
+
+    SDL_Texture *texture =
+        IMG_LoadTexture(Game::GetInstance().GetRenderer(), file.c_str());
+
+    if (!texture) {
+        std::cerr << "Falha ao carregar textura" << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+    
+    imageTable.emplace(file, texture);
+    
+    return texture;
+}
+
+Mix_Music *Resources::GetMusic(const std::string& file) {
+    auto it = musicTable.find(file);
+
+    if (it != musicTable.end()) {
+        return it->second;
+    }
+
+    Mix_Music *music = Mix_LoadMUS(file.c_str());
+    
+    if (!music) {
+        std::cerr << "Falha ao carregar música" << Mix_GetError() << std::endl;
+        return nullptr;
+    }
+    
+    musicTable.emplace(file, music);
+    return music;
+}
+
+Mix_Chunk *Resources::GetSound(const std::string &file) {
+    auto it = soundTable.find(file);
+
+    if (it != soundTable.end()) {
+        return it->second;
+    }
+
+    Mix_Chunk* chunk = Mix_LoadWAV(file.c_str());
+    
+    if (!chunk) {
+        std::cerr << "Falha ao carregar som" << Mix_GetError() << std::endl;
+        return nullptr;
+    }
+    
+    soundTable.emplace(file, chunk);
+    return chunk;
+}
+
+void Resources::ClearImages() {
+    for (auto& image : imageTable) {
+        if (image.second != nullptr) {
+            SDL_DestroyTexture(image.second);
+        }
+    }
+    
+    imageTable.clear();
+}
+
+void Resources::ClearMusics() {
+    for (auto& music : musicTable) {
+        if (music.second != nullptr) {
+            Mix_FreeMusic(music.second);
+        }
+    }
+    
+    musicTable.clear();
+}
+
+void Resources::ClearSounds() {
+    for (auto& sound : soundTable) {
+        if (sound.second != nullptr) {
+            Mix_FreeChunk(sound.second);
+        }
+    }
+    
+    soundTable.clear();
+}
