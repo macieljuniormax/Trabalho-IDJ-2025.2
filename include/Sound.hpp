b70d20721bcddc8d22293a3fe74b87808a8a90d0
Maciel Ferreira Custódio Júnior <maciel_custodio@icloud.com>
2025-10-04 10:20:12 -0300

    :sparkles: feat: implementa classe Sound para reprodução de efeitos sonoros
    - Cria classe Sound
    - Implementa construtores
    - Adiciona métodos Play, Stop eOpen
    - Cria destrutor que libera memória

---
+++ include/Sound.hpp
@@ -0,0 +1,33 @@
+//
+//  Sound.hpp
+//  Trabalho IDJ 2025.2
+//
+//  Created by Maciel Ferreira Custódio Júnior on 03/10/25.
+//
+
+#ifndef Sound_hpp
+#define Sound_hpp
+
+#define INCLUDE_SDL_MIXER
+
+#include "SDL_include.h"
+
+#include <string>
+
+class Sound {
+public:
+    Sound();
+    Sound(const std::string& file);
+    ~Sound();
+    
+    void Play(int times = 1);
+    void Stop();
+    void Open(const std::string file);
+    bool IsOpen() const;
+    
+private:
+    Mix_Chunk* chunk;
+    int channel;
+};
+
+#endif /* Sound_hpp */
---
+++ src/Sound.cpp
@@ -0,0 +1,58 @@
+//
+//  Sound.cpp
+//  Trabalho IDJ 2025.2
+//
+//  Created by Maciel Ferreira Custódio Júnior on 03/10/25.
+//
+
+#include "Sound.hpp"
+
+#include <iostream>
+
+Sound::Sound() : chunk(nullptr) {}
+
+Sound::Sound(const std::string& file) : Sound() {
+    Open(file);
+}
+
+Sound::~Sound() {
+    if (chunk != nullptr) {
+        Mix_HaltChannel(channel);
+        Mix_FreeChunk(chunk);
+        chunk = nullptr;
+    }
+}
+
+void Sound::Play(int times) {
+    if (!chunk) {
+        std::cerr << "Nenhum som carregado" << std::endl;
+        return;
+    }
+    
+    int loops = times > 0 ? times -1 : 0;
+    
+    if ((channel = Mix_PlayChannel(-1, chunk, loops)) == -1) {
+        std::cerr << "Sound::Play: erro ao tocar som: " << Mix_GetError() << std::endl;
+    }
+}
+
+void Sound::Stop() {
+    if (chunk != nullptr  && channel != -1) {
+        Mix_HaltChannel(channel);
+        channel = -1;
+    }
+}
+
+void Sound::Open(const std::string file) {
+    if (chunk != nullptr) {
+        Mix_FreeChunk(chunk);
+        chunk = nullptr;
+    }
+    
+    chunk = Mix_LoadWAV(file.c_str());
+    
+    if (!chunk) {
+        std::cerr << "Erro ao carregar som: " << Mix_GetError() << std::endl;
+    }
+    
+}
